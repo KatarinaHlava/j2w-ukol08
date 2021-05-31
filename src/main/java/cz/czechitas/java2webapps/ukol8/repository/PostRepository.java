7004a9bd3ae47711515f9c43a06dd3e8d3d988ea
KatarinaHlava <k.jurdakova@gmail.com>
2021-05-31 23:10:00 +0200

    povinna sekcia funguje, aspon by ted amala

--- src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/repository/PostRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 @Query
 Post findBySlug(String slug);
 
+Page<Post> findByPublishedBefore(LocalDate datum,Pageable pageable);
 
 }
--- src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
+++ src/main/java/cz/czechitas/java2webapps/ukol8/service/PostService.java
@@ -4,9 +4,11 @@ import cz.czechitas.java2webapps.ukol8.entity.Post;
 import cz.czechitas.java2webapps.ukol8.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -14,17 +16,27 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Autowired
-      public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
-public Page<Post> list(Pageable pageable){
-        return postRepository.findAll(pageable);
+
+    public Page<Post> list(Pageable pageable) {
+        PageRequest.of(0, 20);
+        LocalDate datum = LocalDate.now();
+        return postRepository.findByPublishedBefore(datum, pageable);
+    }
+
+
+    public Post singlePost(String slug) {
+        return postRepository.findBySlug(slug);
+    }
+
 }
-
-
- public Post singlePost(String slug){
-     return postRepository.findBySlug(slug);
- }
- 
-
-}
+//
+//    Uprav metodu list() v PostService tak, aby používala Pageable a omezila výsledek na 20 záznamů.
+//        Pro vytvoření správného Pageable použij statickou metodu PageRequest.of(0, 20).
+//        Vytvoř v repository metodu, která bude vracet Page, bude používat Pageable pro omezení počtu záznamů,
+//        načte pouze posty, které mají datum publikace a není v budoucnosti,
+//        a seřadí záznamy sestupně podle data publikace.
+//        Pro řazení se nebude používat položka sort z Pageable,
+//        ale použije se správný název metody v repository.
